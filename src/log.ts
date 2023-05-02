a0bc1e8840509215345c3222e61b4bddfa98f1c7
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-05-02 23:14:44 +0200

    Moved log to src root

--- src/log.ts
+++ src/log.ts
@@ -1,19 +1,21 @@
 import debug from "debug";
 
 export default class Log {
+  private static _prefix: string = "[Cypress-SignalR-Mock] - ";
+
   public static debug(message: string) {
     debug("cypress-signalr-mock")(message);
   }
 
   public static info(message: string) {
-    console.info(message);
+    console.info(this._prefix + message);
   }
 
   public static warn(message: string) {
-    console.warn(message);
+    console.warn(this._prefix + message);
   }
 
   public static error(message: string) {
-    console.error(message);
+    console.error(this._prefix + message);
   }
 }
