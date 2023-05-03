0d709484748846ca647fe43db5394a65a56ff076
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-05-03 17:21:24 +0200

    Moved functions to utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,24 @@
+import HubConnectionMock from "./types/HubConnectionMock.ts";
+import IMockData from "./types/IMockData.ts";
+import Log from "./log.ts";
+
 export function isCypressRunning(): boolean {
   return window.hasOwnProperty("Cypress");
+}
+
+export function getCypressSignalrMockData(): IMockData {
+  if (window.hasOwnProperty("cypress-signalr-mock")) {
+    return window["cypress-signalr-mock"];
+  }
+  // This throws an error in the Cypress console
+  Log.error('window["cypress-signalr-mock"] is not initialized.');
+  return {} as IMockData;
+}
+
+export function getHubConnectionMock(
+  hubName: string
+): HubConnectionMock | null {
+  return (
+    getCypressSignalrMockData().mocks.find((x) => x.name === hubName) || null
+  );
 }
