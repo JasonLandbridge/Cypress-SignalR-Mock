12192a58e2869d38a0b7f948b2195db4073a1adb
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-05-04 17:10:00 +0200

    feat(Playground): Added Cypress commands to run with Nuxt in the playground package.json

--- playground/cypress.config.ts
+++ playground/cypress.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    viewportHeight: 1080,
+    viewportWidth: 1920,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
