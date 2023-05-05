9c2c5c148be49d73ce7fd8a4862caf671cc7cf92
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-05-05 11:20:49 +0200

    test(Playground): Added simple cy.hubPublish demo

--- playground/app.vue
+++ playground/app.vue
@@ -3,3 +3,20 @@
     <NuxtWelcome />
   </div>
 </template>
+<script setup lang="ts">
+import { useCypressSignalRMock } from "../src";
+import { HubConnectionBuilder } from "@microsoft/signalr";
+
+const connection =
+  useCypressSignalRMock("demo-hub") ??
+  new HubConnectionBuilder()
+    .withUrl(`http://localhost:5000/demo-hub`)
+    .withAutomaticReconnect()
+    .build();
+
+connection.on("tasks", (message) => {
+  console.log(message);
+});
+
+console.log("demo-hub signalR has been set-up!");
+</script>
--- playground/cypress/e2e/hub-publish.cy.ts
+++ playground/cypress/e2e/hub-publish.cy.ts
@@ -1,5 +1,14 @@
 describe("cy.hubPublish demo", () => {
   it("passes", () => {
     cy.visit("http://localhost:3030/");
+    // We wait here to ensure the SignalR has had time to be set-up during page-load
+    // Normally, we would await page-load before using xy.hubPublish
+    cy.wait(1000).then(() => {
+      cy.hubPrintData();
+      cy.hubPublish("demo-hub", "tasks", {
+        id: "1",
+        name: "Task 1",
+      });
+    });
   });
 });
