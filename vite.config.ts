6cbaa3db65452e7a6ab2c6088502c328594dd6fc
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-05-05 11:24:19 +0200

    build(Cypress-SignalR-Mock): Changed the main dist file to a variation of index

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
     sourcemap: true,
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
-      name: "cypress-signalr-mock",
+      name: "index",
+      fileName: "index",
     },
   },
   plugins: [dts()],
