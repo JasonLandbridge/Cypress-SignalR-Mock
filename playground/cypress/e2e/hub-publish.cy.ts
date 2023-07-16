1c67c1875c9e7268a7536e53c9188941e3ce4cb2
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-07-16 17:43:07 +0200

    ci(Cypress-SignalR-Mock): Added Timeout

--- playground/cypress/e2e/hub-publish.cy.ts
+++ playground/cypress/e2e/hub-publish.cy.ts
@@ -2,7 +2,7 @@ describe("cy.hubPublish demo", () => {
   it("Should keep sending progress when cy.hubPublish is sent multiple times", () => {
     cy.visit("http://localhost:3030/");
     // page-load-completed is a data-cy attribute that is added in the layouts/default.vue file
-    cy.get('[data-cy="page-load-completed"]').then(() => {
+    cy.get('[data-cy="page-load-completed"]', { timeout: 10000 }).then(() => {
       // Mock the usage of a progress bar
       Cypress._.times(11, (i) => {
         cy.hubPublish("demo-hub", "progress", i * 10);
