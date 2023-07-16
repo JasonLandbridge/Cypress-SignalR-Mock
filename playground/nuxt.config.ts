778da471e568ea87b96c8ecc1d9627229d951e34
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-07-16 16:00:03 +0200

    fix(Playground): Fixed the "[nuxt] [request error] [unhandled] [500] Cannot find module './internal/Observable'" that happens on running the playground app

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -3,4 +3,9 @@ import { defineNuxtConfig } from "nuxt/config";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: true,
+  build: {
+    // This fixes the error: "[nuxt] [request error] [unhandled] [500] Cannot find module './internal/Observable'"
+    // This most likely is only needed when Cypress-SignalR-Mock is referenced via a relative path
+    transpile: ["rxjs"],
+  },
 });
