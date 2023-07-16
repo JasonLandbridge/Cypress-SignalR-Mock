f872507b40ec9f5131984fea78c594f60ffe76d5
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-07-16 17:29:27 +0200

    test(Cypress-SignalR-Mock): Cleanup Cypress Test

--- playground/cypress/e2e/hub-publish.cy.ts
+++ playground/cypress/e2e/hub-publish.cy.ts
@@ -1,16 +1,8 @@
 describe("cy.hubPublish demo", () => {
-  beforeEach(() => {
-    cy.visit("http://localhost:3030/");
-  });
   it("Should keep sending progress when cy.hubPublish is sent multiple times", () => {
+    cy.visit("http://localhost:3030/");
     // page-load-completed is a data-cy attribute that is added in the layouts/default.vue file
     cy.get('[data-cy="page-load-completed"]').then(() => {
-      cy.hubPrintData();
-      cy.hubPublish("demo-hub", "tasks", {
-        id: "1",
-        name: "Task 1",
-      });
-
       // Mock the usage of a progress bar
       Cypress._.times(11, (i) => {
         cy.hubPublish("demo-hub", "progress", i * 10);
